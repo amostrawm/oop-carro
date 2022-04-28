a09306613592130904e81596a15a5e94d8a94c1a
Amós Almeida <amsbeawp@gmail.com>
2022-04-28 12:13:50 -0300

    treino oop

---
+++ company.py
@@ -0,0 +1,30 @@
+from employee import Employee
+
+
+class Company:
+    def __init__(self, name, country, sector):
+        self.name = name
+        self.country = country
+        self.sector = sector
+
+    def compdata(self):
+        return 'Name: {}\n' \
+               'Country: {}\n' \
+               'Sector: {}\n'.format(self.name, self.country, self.sector)
+
+    def emp_comp(self, company, emp):
+        if company == emp:
+            return '{} works on {}'.format(emp, company)
+        else:
+            return 'Error'
+
+
+emp_1 = Employee('André', 'Marques', 4000, '3M')
+emps = [emp_1]
+print('Name: ' + emps[0].fullname() + '\n' + emps[0].empdata())
+
+comp_1 = Company('3M', 'Usa', 'Production')
+comps = [comp_1]
+print(comps[0].compdata())
+
+Company.emp_comp(emp_1.company, comp_1.name)
--- employee.py
+++ employee.py
@@ -1,20 +1,17 @@
 class Employee:
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last, pay, company):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
+        self.company = company
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
-    def alldata(self):
+    def empdata(self):
         return 'First name: {}\n' \
                'Last name: {}\n' \
                'Pay: {}\n' \
-               'Email: {}\n'.format(self.first, self.last, self.pay, self.email)
-
-
-emp_1 = Employee('André', 'Marques', 4000)
-print('Name: ' + emp_1.fullname())
-print(emp_1.alldata())
+               'Company: {}\n' \
+               'Email: {}\n'.format(self.first, self.last, self.pay, self.company,self.email)
