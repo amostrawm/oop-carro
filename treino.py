47a8bf0f830e90b6ec29eddbc6c736d332e7c9d9
Amós Almeida <amsbeawp@gmail.com>
2022-04-28 11:45:51 -0300

    treino oop

--- treino.py
+++ treino.py
@@ -1,14 +1,14 @@
-class Carro():
+class Carro:
     def __init__(self, marca, nome, cor, ano):
         self.marca = marca
         self.nome = nome
         self.cor = cor
         self.ano = ano
 
-    def Ligar(self):
+    def ligar(self):
         print('O carro está ligando!')
 
-    def Desligar(self):
+    def desligar(self):
         print('O carro está desligado')
 
 
--- treinocarro.py
+++ treinocarro.py
@@ -2,4 +2,4 @@ from treino import Carro
 
 carro1 = Carro('Chevrollet', 'Celta', 'Azul', '2005')
 
-carro1.Ligar()
+carro1.ligar()
