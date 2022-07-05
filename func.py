5e6dc8119fad797a12a3b68ec534701b96a49c1e
Amós Almeida <amsbeawp@gmail.com>
2022-07-05 20:26:49 -0300

    treino oop

---
+++ func.py
@@ -0,0 +1,16 @@
+def Sum(numbers):
+    for x in numbers:
+        n1 = x
+        print(n1)
+
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
+Numbers = [0, 1, 2]
+
+Sum(Numbers)
